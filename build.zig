25bcf9633f530098267f9b2aa43b6c8792c6bb4c
Tim Hilt <timhilt@live.de>
2025-01-03 16:16:53 +0100

    remove zig-out dir

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
-        .name = "blink",
+        .name = "blink.elf",
         .root_source_file = b.path("src/main.zig"),
         .target = b.resolveTargetQuery(.{
             .cpu_arch = .thumb,
